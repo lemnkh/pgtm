83209c4aea41db6a23441b7defef629d193da46d
lemnkh <ahlemkhattab@gmail.com>
2020-03-24 19:25:58 +0100

    hopefully this fixes it

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(require('node-sass-middleware')({
 
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // express session
